b50c62189ec32d582a94dcb1042f93e3340750a7
James Brooking <jambroo@gmail.com>
2016-01-25 12:04:05 +0100

    Json progress.

--- middleground/json.py
+++ middleground/json.py
@@ -4,6 +4,23 @@ class Json:
     def __init__(self, data):
         self.data = json.loads(data)
 
+    def to_str(self, deserialized_json):
+        out = ""
+        if isinstance(deserialized_json, dict):
+            for key in sorted(deserialized_json):
+                print(key)
+                out += self.to_str(deserialized_json[key]) + " "
+            return out
+
+        if isinstance(deserialized_json, list):
+            for item in deserialized_json:
+                out += self.to_str(item) + " "
+            return out
+
+        if isinstance(deserialized_json, str):
+            return deserialized_json
+        else:
+            return ""
+
     def __str__(self):
-        # TODO: Remove JSON structural characters
-        return str(self.data)
+        return self.to_str(self.data)
