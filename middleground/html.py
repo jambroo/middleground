f324bdcfee9618a12894fdd168e8f4f60f1cc8f7
James Brooking <jambroo@gmail.com>
2016-01-26 11:22:49 +0100

    PR comment implemented.

--- middleground/html.py
+++ middleground/html.py
@@ -2,9 +2,8 @@ import html2text
 
 class Html:
     def __init__(self, data):
-        if data == None or data == "":
-            self.data = ""
-        else:
+        self.data = ""
+        if data != None and data != "":
             h = html2text.HTML2Text()
             h.ignore_links = True
             self.data = h.handle(data).strip()
