56673d6436be07e960c3d707c17c8c5cba7a8eb3
James Brooking <jambroo@gmail.com>
2016-01-26 12:38:59 +0100

    More complicated test for ODT added.

--- tests/test_odt.py
+++ tests/test_odt.py
@@ -10,5 +10,12 @@ class TestOdt(unittest.TestCase):
             odt = Odt(odtRaw)
         self.assertEqual(str(odt), "Test Odt!")
 
+    def test_odt2(self):
+        pdf = None
+        with open("tests/test2.odt", 'rb') as odtF:
+            odtRaw = odtF.read()
+            odt = Odt(odtRaw)
+        self.assertTrue("This appendix is based on Appendix B of Getting Started with OpenOffice.org." in str(odt))
+
 if __name__ == '__main__':
     unittest.main()
