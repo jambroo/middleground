43b5910203e641185d935972215257b17216301f
James Brooking <jambroo@gmail.com>
2016-01-28 13:00:36 +0100

    Added tests for Read class.

--- tests/test_docx.py
+++ tests/test_docx.py
@@ -1,22 +1,36 @@
 import unittest
-from middleground.docx import Docx
+from middleground.read import Read
+import config
 
 class TestDocx(unittest.TestCase):
-    def test_docx_simple(self):
-        testF = open("tests/files/test.docx", "rb")
-        docx = Docx(testF.read())
-        testF.close()
+    def test_docx_simple_path(self):
+        path = "tests/files/test.docx"
+        read = Read(path=path)
+        self.assertEqual(str(read), "Hello world")
 
-        docxStr = str(docx)
-        self.assertEqual(docxStr, "Hello world")
+    def test_docx_simple_content(self):
+        testF = open("tests/files/test.docx", "rb")
+        read = Read(content=testF.read())
+        testF.close()
+        self.assertEqual(str(read), "Hello world")
 
     def test_docx(self):
         testF = open("tests/files/test2.docx", "rb")
-        docx = Docx(testF.read())
+        read = Read(content=testF.read())
         testF.close()
+        self.assertEqual(str(read)[0:29], '12\nEnglish\t\t\t\t\t\t\t\t\t\t\tDrawings')
 
-        docxStr = str(docx)
-        self.assertEqual(docxStr[0:40], "Description\tTitle of Invention : Sample ")
+    def test_doc_simple(self):
+        testF = open("tests/files/test.doc", "rb")
+        read = Read(content=testF.read())
+        testF.close()
+        self.assertEqual(str(read), "Hello world")
+
+    def test_doc(self):
+        testF = open("tests/files/test2.doc", "rb")
+        read = Read(content=testF.read())
+        testF.close()
+        self.assertEqual(str(read)[0:36], 'Sample document created with MS Word')
 
 
 if __name__ == '__main__':
