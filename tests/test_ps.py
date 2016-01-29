b2ae5062f09852d88f22bbeebf463e43016fb8e9
James Brooking <jambroo@gmail.com>
2016-01-29 17:29:29 +0100

    Msg file works but PS file still WIP.

---
+++ tests/test_ps.py
@@ -0,0 +1,15 @@
+import unittest
+from middleground.read import Read
+import config
+
+class TestPs(unittest.TestCase):
+    def test_ps(self):
+        # WIP
+        pass
+    #     read = Read(path="tests/files/test.ps")
+    #     self.assertEqual(str(read), 'Test')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
