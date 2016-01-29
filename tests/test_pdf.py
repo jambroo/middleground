4dbe55052f947e2375289a3e00cf466591833022
James Brooking <jambroo@gmail.com>
2016-01-29 16:30:19 +0100

    Cleaning up unneeded classes.

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,12 +1,13 @@
 import unittest
 from middleground.pdf import Pdf
+from middleground.read import Read
 
 class TestPdf(unittest.TestCase):
     def test_pdf(self):
         pdf = None
         with open("tests/files/test.pdf", 'rb') as pdfF:
             pdfRaw = pdfF.read()
-            pdf = Pdf(pdfRaw)
+            pdf = Read(content=pdfRaw)
         self.assertEqual(str(pdf), "hello world")
 
     def test_long_pdf(self):
