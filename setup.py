f4d2445a89a0dd2253772d9eab3ed91a24d389a8
James Brooking <jambroo@gmail.com>
2016-02-05 14:51:12 +0100

    Added module setup script.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup(name='middleground',
+      version='0.1',
+      description='middleground is a Python module for converting various '+
+        'document types to a normalised plaintext using am Apache Tika server.',
+      packages=['middleground'],
+      install_requires=[
+          'ezodf',
+          'html2text',
+          'odfpy',
+          'pdfminer3k',
+          'ply',
+          'py',
+          'pytest',
+          'wheel',
+          'xlrd'
+      ])
