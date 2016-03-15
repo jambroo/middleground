b248dad45f14eb2bf94cf98dbcf244c7a2dbfe26
James Brooking <jambroo@gmail.com>
2016-03-15 11:19:49 +0100

    Test PNG files.

--- tests/test_png.py
+++ tests/test_png.py
@@ -1,9 +1,10 @@
 import unittest
 from middleground.read import Read
 
+
 class TestPng(unittest.TestCase):
     def test_png(self):
-        png = Read(path="tests/files/test.png")
+        png = Read(path="tests/files/meta.png")
         pngStr = str(png)
         self.assertTrue("BRISBANE" in pngStr)
 
