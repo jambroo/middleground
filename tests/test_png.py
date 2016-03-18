3b581e9242dfb6cee5648a7fafde46586a948ff1
James Brooking <jambroo@gmail.com>
2016-03-18 13:39:23 +0100

    Rest of tests fixed.

--- tests/test_png.py
+++ tests/test_png.py
@@ -1,10 +1,10 @@
 import unittest
 from middleground.read import Read
-
+from test_config import TIKA_SERVER
 
 class TestPng(unittest.TestCase):
     def test_png(self):
-        png = Read(path="tests/files/meta.png")
+        png = Read(path="tests/files/meta.png", tikaServer=TIKA_SERVER)
         pngStr = str(png)
         self.assertTrue("BRISBANE" in pngStr)
 
--- tests/test_ppt.py
+++ tests/test_ppt.py
@@ -1,15 +1,15 @@
 import unittest
 from middleground.read import Read
-
+from test_config import TIKA_SERVER
 
 class TestPpt(unittest.TestCase):
     def test_ppt(self):
-        read = Read(path="tests/files/test.ppt")
+        read = Read(path="tests/files/test.ppt", tikaServer=TIKA_SERVER)
         self.assertEqual(str(read)[0:20], "Test PowerPoint FIle")
 
 
     def test_pptx(self):
-        read = Read(path="tests/files/test.pptx")
+        read = Read(path="tests/files/test.pptx", tikaServer=TIKA_SERVER)
         self.assertEqual(str(read)[0:34], "PowerPoint Presentation\n\n\nUTK Quiz")
 
 
